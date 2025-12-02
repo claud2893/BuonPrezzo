946d2b0ce4709bf597447383505f2df4e4289da5
claud2893 <claudio.monopoli.2893@gmail.com>
2025-12-02 11:21:13 +0100

    stage

--- database_prezzi.py
+++ database_prezzi.py
@@ -10,143 +10,199 @@ from selenium.webdriver.support import expected_conditions as EC
 def database_prezzi():
 
     options = Options()
-    options.add_argument("--headless=new")
+    # options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
 
     dati_prodotti = []
 
-    URL2 = "https://osservaprezzi.mise.gov.it/prezzi/livelli/beni-e-servizi-di-largo-consumo/archivio-rilevazioni-beni-e-servizi-di-largo-consumo"
+    # categorie = ["501", "558", "590", "2136", "721", "754", "789", "821", "684", "652", "928"]
 
-    categorie = []
+    # for categoria in categorie:
 
-    ortaggi_URL = "501"
-    categorie.append(ortaggi_URL)
-    frutta_URL = "558"
-    categorie.append(frutta_URL)
-    agrumi_URL = "590"
-    categorie.append(agrumi_URL)
-    frutta_guscio_URL = "2136"
-    categorie.append(frutta_guscio_URL)
-    carne_bovina_URL = "721"
-    categorie.append(carne_bovina_URL)
-    carne_suina_salumi_URL = "754"
-    categorie.append(carne_suina_salumi_URL)
-    avicoli_uova_URL = "789"
-    categorie.append(avicoli_uova_URL)
-    ovicaprini_URL = "821"
-    categorie.append(ovicaprini_URL)
-    latte_bovino_URL = "684"
-    categorie.append(latte_bovino_URL)
-    olio_oliva_URL = "652"
-    categorie.append(olio_oliva_URL)
-    cereali_URL = "853"
-    categorie.append(cereali_URL)
-    semi_oleosi_URL = "928"
-    categorie.append(semi_oleosi_URL)
+    #     URL = f"https://www.ismeamercati.it/flex/cm/pages/ServeBLOB.php/L/IT/IDPagina/{categoria}"
 
-    for categoria in categorie:
+    #     try:
+    #         driver.get(URL)
 
-        URL = f"https://www.ismeamercati.it/flex/cm/pages/ServeBLOB.php/L/IT/IDPagina/{categoria}"
+    #         try:
+    #             wait = WebDriverWait(driver, 5)
+    #             rifiuta_cookies = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'eu-privacy-button-close')))
+    #             rifiuta_cookies.click()
+    #         except:
+    #             pass
 
+    #         tabella = driver.find_element(By.TAG_NAME, 'tbody')
+    #         dati = tabella.find_elements(By.TAG_NAME, 'tr')
+
+    #         for elemento in dati:
+    #             prodotto = elemento.text.split()
+    #             nome = prodotto[0]
+    #             index = 1
+    #             while prodotto[index].isalpha():
+    #                 nome = nome + " " + prodotto[index]
+    #                 index += 1
+    #             if prodotto[-3] == "vivo" or prodotto[-3] == "unitÃ" or prodotto[-3] == "kg":
+    #                 unita = prodotto[-4].replace("€/","")
+    #                 prezzo = float(prodotto[-5].replace(",","."))
+    #                 if unita == "Kg/peso":
+    #                     unita = "kg"
+    #                 elif unita == "100":
+    #                     prezzo = round((float(prodotto[-5].replace(",",".")) / 100), 2)
+    #                     unita = 1
+    #             else:
+    #                 unita = prodotto[-3].replace("€/","")
+    #                 if unita == "100":
+    #                     prezzo = round((float(prodotto[-4].replace(",",".")) / 100), 2)
+    #                     unita = 1
+    #                 elif unita == "Capolino":
+    #                     prezzo = float(prodotto[-4].replace(",","."))
+    #                     unita = 1
+    #                 else:
+    #                     prezzo = float(prodotto[-4].replace(",","."))
+    #             try:
+    #                 dati_prodotti.append({"nome" : nome, "prezzo" : prezzo, "unità" : unita.lower()})
+    #             except:
+    #                 dati_prodotti.append({"nome" : nome, "prezzo" : prezzo, "unità" : unita})
+
+    #     except Exception as e:
+    #         print(f"Errore primo scraping: {e}")
+
+    # URL2 = "https://osservaprezzi.mise.gov.it/prezzi/livelli/beni-e-servizi-di-largo-consumo/archivio-rilevazioni-beni-e-servizi-di-largo-consumo"
+
+    # try:
+    #     driver.get(URL2)
+    #     t.sleep(2)
+    #     driver.fullscreen_window()
+    #     driver.set_window_size(1920, 1080)
+
+    #     tabella = driver.find_element(By.TAG_NAME, 'tbody')
+    #     dati = tabella.find_elements(By.TAG_NAME, 'tr')
+
+    #     for elemento in dati:
+    #         prodotto = elemento.text.split()
+    #         prezzo = float(prodotto[-2])
+    #         nome = prodotto[0]
+    #         index = 1
+    #         while prodotto[index].isalpha():
+    #             nome = nome + " " + prodotto[index]
+    #             index += 1
+    #         if prodotto[index].startswith("("):
+    #             quantita = prodotto[index].replace("(","")
+    #             index += 1
+    #             unita = prodotto[index].replace(")","")
+    #         if quantita == "1000" and unita == "Gr":
+    #             unita = "kg"
+    #         elif quantita == "300" and unita == "Gr":
+    #             prezzo = round(((prezzo / 3) * 10), 2)
+    #             unita = "kg"
+    #         elif quantita == "100" and unita == "Cl":
+    #             unita = "l"
+    #         elif quantita == "125" and unita == "Gr":
+    #             prezzo = prezzo * 8
+    #             unita = "kg"
+    #         elif quantita == "1000" and unita == "Ml":
+    #             unita = "l"
+    #         elif quantita == "100" and unita == "Gr":
+    #             prezzo = prezzo * 10
+    #             unita = "kg"
+    #         elif quantita == "900" and unita == "Cl":
+    #             prezzo = round(((prezzo / 9) * 10), 2)
+    #             unita = "l"
+    #         elif quantita == "75" and unita == "Cl":
+    #             prezzo = round(((prezzo / 3) * 4), 2)
+    #             unita = "l"
+
+    #         dati_prodotti.append({"nome" : nome, "prezzo" : prezzo, "unità" : unita.lower()})
+                                   
+    # except Exception as e:
+    #     print(f"Errore secondo scraping: {e}")
+
+    sezioni = ["liquori-e-distillati_5405", "birra_5402", "aperitivi_5401", "riso_5383", 
+               "pasta-secca_5382", "pasta-fresca_5381", "lievito_5380", "yogurt_5375", 
+               "uova_5374", "latte_5368", "burro-e-altri-derivati_5373", "salumi_5370", 
+               "formaggi_5372", "pesce_5371", "carne_5361", "verdura_5367", 
+               "legumi-semi-e-cereali_5366", "frutta_5365", "farina_5379", 
+               "liquori-e-distillati_5405", "succhi-e-bibite_5408", "vino_5410"]
+
+   
+    for sezione in sezioni:
+        
+        URL3 = f"https://pamacasa.pampanorama.it/{sezione}"
+        
         try:
-            driver.get(URL)
-
+            driver.get(URL3)
+            
             try:
                 wait = WebDriverWait(driver, 5)
-                rifiuta_cookies = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'eu-privacy-button-close')))
+                rifiuta_cookies = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'iubenda-cs-cwa-button')))
                 rifiuta_cookies.click()
+                t.sleep(5)
             except:
                 pass
 
-            tabella = driver.find_element(By.TAG_NAME, 'tbody')
-            dati = tabella.find_elements(By.TAG_NAME, 'tr')
+            footer = driver.find_element(By.LINK_TEXT, 'Accessibilità')
+            lista = driver.find_element(By.ID, 'product-grid')
+            items = lista.find_elements(By.CLASS_NAME, 'list-item')
 
-            for elemento in dati:
-                prodotto = elemento.text.split()
-                nome = prodotto[0]
-                index = 1
-                while prodotto[index].isalpha():
-                    nome = nome + " " + prodotto[index]
-                    index += 1
-                if prodotto[-3] == "vivo" or prodotto[-3] == "unitÃ" or prodotto[-3] == "kg":
-                    unita = prodotto[-4].replace("€/","")
-                    prezzo = float(prodotto[-5].replace(",","."))
-                    if unita == "Kg/peso":
-                        unita = "Kg"
-                    elif unita == "100":
-                        prezzo = round((float(prodotto[-5].replace(",",".")) / 100), 2)
-                        unita = 1
-                    dati_prodotti.append({"nome" : nome, "prezzo" : prezzo, "unità" : unita.lower()})
-                else:
-                    unita = prodotto[-3].replace("€/","")
-                    if unita == "100":
-                        prezzo = round((float(prodotto[-4].replace(",",".")) / 100), 2)
-                        unita = 1
-                    elif unita == "T":
-                        prezzo = round((float(prodotto[-4].replace(",",".")) / 1000), 2)
-                        unita = "Kg"
-                    elif unita == "Capolino":
-                        prezzo = float(prodotto[-4].replace(",","."))
-                        unita == 1
+            index = 0
+            while index < 10:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")                
+                index += 1
+                t.sleep(0.5)
+
+
+            # nome_ultimo = items.pop().find_element(By.TAG_NAME, 'h3').text 
+
+            # print(f"Test: {nome_ultimo}")
+            item_index = 0
+            Error = False
+            while True:
+                try:
+                    if item_index != 0 and Error:
+                        for i in range(item_index, len(items)):
+                            nome = items[i].find_element(By.TAG_NAME, 'h3').text.title()
+                            field = items[i].find_elements(By.CLASS_NAME, 'product-meta')
+                            infos = field[1].text.split()
+
+                            prezzo = float(infos[0].replace(",","."))
+                            unita = infos[-1]
+                            if unita == "litro":
+                                unita = "l"
+
+                            item_index = index
+                            print({"nome" : nome, "prezzo" : prezzo, "unità" : unita})
                     else:
-                        prezzo = float(prodotto[-4].replace(",","."))
-                    dati_prodotti.append({"nome" : nome, "prezzo" : prezzo, "unità" : unita.lower()})
+                        for index, item in enumerate(items):
+                            nome = item.find_element(By.TAG_NAME, 'h3').text.title()
+                            field = item.find_elements(By.CLASS_NAME, 'product-meta')
+                            infos = field[1].text.split()
+
+                            prezzo = float(infos[0].replace(",","."))
+                            unita = infos[-1]
+                            if unita == "litro":
+                                unita = "l"
+
+                            item_index = index
+                            print({"nome" : nome, "prezzo" : prezzo, "unità" : unita})
+                except:
+                    Error = True
+                    t.sleep(2)
+                    lista = driver.find_element(By.ID, 'product-grid')
+                    items = lista.find_elements(By.CLASS_NAME, 'list-item')
+                    continue
+                if item_index == len(items):
+                    break
+
 
         except Exception as e:
-            print(f"Errore primo scraping: {e}")
+            print(f"Errore terzo scraping: {e}")
 
-    try:
-        driver.get(URL2)
-        t.sleep(2)
-        driver.fullscreen_window()
-        driver.set_window_size(1920, 1080)
+        finally:
+            driver.quit()
 
-        tabella = driver.find_element(By.TAG_NAME, 'tbody')
-        dati = tabella.find_elements(By.TAG_NAME, 'tr')
-
-        for elemento in dati:
-            prodotto = elemento.text.split()
-            prezzo = float(prodotto[-2])
-            nome = prodotto[0]
-            index = 1
-            while prodotto[index].isalpha():
-                nome = nome + " " + prodotto[index]
-                index += 1
-            if prodotto[index].startswith("("):
-                quantita = prodotto[index].replace("(","")
-                index += 1
-                unita = prodotto[index].replace(")","")
-            if quantita == "1000" and unita == "Gr":
-                unita = "Kg"
-            elif quantita == "300" and unita == "Gr":
-                prezzo = round(((prezzo / 3) * 10), 2)
-                unita = "Kg"
-            elif quantita == "100" and unita == "Cl":
-                unita = "L"
-            elif quantita == "125" and unita == "Gr":
-                prezzo = prezzo * 8
-                unita = "Kg"
-            elif quantita == "1000" and unita == "Ml":
-                unita = "L"
-            elif quantita == "100" and unita == "Gr":
-                prezzo = prezzo * 10
-                unita = "Kg"
-            elif quantita == "900" and unita == "Cl":
-                prezzo = round(((prezzo / 9) * 10), 2)
-                unita = "L"
-            elif quantita == "75" and unita == "Cl":
-                prezzo = round(((prezzo / 3) * 4), 2)
-                unita = "L"
-
-            dati_prodotti.append({"nome" : nome, "prezzo" : prezzo, "unità" : unita.lower()})
-                                   
-    except Exception as e:
-        print(f"Errore secondo scraping: {e}")
-
-    finally:
-        driver.quit()
-
-    with open("dati_prodotti.json", "w", encoding="utf-8") as f:
-        json.dump(dati_prodotti, f,indent=4, ensure_ascii=False)
+    # with open("dati_prodotti.json", "w", encoding="utf-8") as f:
+    #     json.dump(dati_prodotti, f,indent=4, ensure_ascii=False)
 
     return dati_prodotti
+
+print(database_prezzi())
